ca6d6ec58c665e2a8d9b7c3c72e635247341e916
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2017-10-24 23:26:08 +0600

    Faculty Area Add

---
+++ Result Management System/FacultyArea.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Result_Management_System
+{
+    public partial class FacultyArea : Form
+    {
+        public FacultyArea()
+        {
+            InitializeComponent();
+        }
+
+        private void FacultyArea_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
